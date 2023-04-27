c400b1adb94417f254384658841f1c60ca51a022
Kevin Perez <kp952547@gmail.com>
2023-04-27 12:28:15 -0600

    Initial Commit

---
+++ SwiftFlow/SwiftFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFlowApp.swift
+//  SwiftFlow
+//
+//  Created by Kevin Perez on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
